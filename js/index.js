159d953257b5a8d9bc00144ef9c18e03a4ee742d
Andreas Nilsson <andreas.nilsson333@gmail.com>
2016-10-22 10:10:30 +0100

    refactor code
    - remove DOMContentLoaded event
    - Use functions for handling keyup/keydown
    - store accepted keyCodes in an object

--- js/index.js
+++ js/index.js
@@ -1,92 +1,101 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var keys = [];
+var canvas = document.getElementById('canvas');
+canvas.width = 600;
+canvas.height = 400;
 
-	var canvas = document.getElementById('canvas');
-	canvas.width = 600;
-	canvas.height = 400;
-	
-	var ctx = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
 
-	var movingObject = {};
-	movingObject.posX = 0;
-	movingObject.posY = 0;
-	movingObject.height = 50;
-	movingObject.width = 50;
-	movingObject.speed = 5;
+var movingObject = {
+	posX: 0,
+	posY: 0,
+	height: 50,
+	width: 50;
+	speed: 5
+};
 
-	document.addEventListener('keydown', function (e) {
-		if (e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 40) {
-			e.preventDefault();
+var arrowKeys = {
+	37: 'left',
+	38: 'up',
+	39: 'right',
+	40: 'down'
+};
 
-			var addKey = true;
-			keys.forEach(function (key) {
-				if (e.keyCode === key) {
-					addKey = false;
-				}
-			});
-			if (addKey) {
-				keys.push(e.keyCode);
+var currentKeys = [];
+
+function handleKeyDown (e) {
+	if (arrowKeys[e.keyCode]) {
+		e.preventDefault();
+
+		var addKey = true;
+
+		currentKeys.forEach(function (key) {
+			if (e.keyCode === key) {
+				addKey = false;
 			}
+		});
+
+		if (addKey) {
+			currentKeys.push(e.keyCode);
 		}
-	});
+	}
+}
 
-	document.addEventListener('keyup', function (e) {
-		if (e.keyCode === 37 || e.keyCode === 38 || e.keyCode === 39 || e.keyCode === 40) {
-			e.preventDefault();
+function handleKeyUp (e) {
+	if (arrowKeys[e.keyCode]) {
+		e.preventDefault();
 
-			keys.forEach(function (key, i) {
-				if (e.keyCode === key) {
-					keys.splice(i, 1);
-				}
-			});
-		}
-	});
-
-	
-
-	function update() {
-		keys.forEach(function (key) {
-			switch(key) {
-				case 37:
-					// left
-					if (movingObject.posX > 0) {
-						movingObject.posX -= movingObject.speed;
-					}
-					break;
-				case 38:
-					// up
-					if (movingObject.posY > 0) {
-						movingObject.posY -= movingObject.speed;
-					}
-					break;
-				case 39:
-					// right
-					if (movingObject.posX < canvas.width - movingObject.width) {
-						movingObject.posX += movingObject.speed;
-					}
-					break;
-				case 40:
-					// down
-					if (movingObject.posY < canvas.height - movingObject.height) {
-						movingObject.posY += movingObject.speed;
-					}
-					break;
+		currentKeys.forEach(function (key, i) {
+			if (e.keyCode === key) {
+				currentKeys.splice(i, 1);
 			}
 		});
 	}
+}
 
-	function render() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup', handleKeyUp);
 
-		ctx.fillStyle = 'red';
-		ctx.fillRect(movingObject.posX, movingObject.posY, movingObject.width, movingObject.height);
-	}
+function update() {
+	currentKeys.forEach(function (key) {
+		switch(key) {
+			case 37:
+				// left
+				if (movingObject.posX > 0) {
+					movingObject.posX -= movingObject.speed;
+				}
+				break;
+			case 38:
+				// up
+				if (movingObject.posY > 0) {
+					movingObject.posY -= movingObject.speed;
+				}
+				break;
+			case 39:
+				// right
+				if (movingObject.posX < canvas.width - movingObject.width) {
+					movingObject.posX += movingObject.speed;
+				}
+				break;
+			case 40:
+				// down
+				if (movingObject.posY < canvas.height - movingObject.height) {
+					movingObject.posY += movingObject.speed;
+				}
+				break;
+		}
+	});
+}
 
-	function gameLoop() {
-		update();
-		render();
-		requestAnimationFrame(gameLoop);
-	}
+function render() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+	ctx.fillStyle = 'red';
+	ctx.fillRect(movingObject.posX, movingObject.posY, movingObject.width, movingObject.height);
+}
+
+function gameLoop() {
+	update();
+	render();
 	requestAnimationFrame(gameLoop);
-});
+}
+
+requestAnimationFrame(gameLoop);
